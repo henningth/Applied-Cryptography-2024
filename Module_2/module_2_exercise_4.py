082f44cc564a5768c357ecd142d9327f0318d2f5
Henning Thomsen <ht1703@gmail.com>
2024-02-21 12:38:38 +0100

    Added padding

--- Module_2/module_2_exercise_4.py
+++ Module_2/module_2_exercise_4.py
@@ -20,10 +20,17 @@ else:
     padded_key = padder.update(key)
     padded_key += padder.finalize()
     key = padded_key 
-        
-print("key", key)
-message = b'Hello World, heyHello World, hey' # Message is 16 bytes
-cipher = Cipher(algorithm=algorithms.AES(key), mode=modes.ECB())
+
+def add_padding(message):
+    padder = padding.PKCS7(128).padder()
+    padded_message = padder.update(message)
+    padded_message += padder.finalize()
+    return padded_message
+
+def remove_padding(padded_message):
+    unpadder = padding.PKCS7(128).unpadder()
+    unpadded_message = unpadder.update(padded_message)
+    return unpadded_message
 
 def encrypt(message):
     encryptor = cipher.encryptor()
@@ -31,14 +38,28 @@ def encrypt(message):
     ciphertext += encryptor.finalize()
     return ciphertext
 
-ciphertext = encrypt(message)
-print(ciphertext)
-
 def decrypt(ciphertext):
     decryptor = cipher.decryptor()
     decryptedmessage = decryptor.update(ciphertext)
     decryptedmessage += decryptor.finalize()
     return decryptedmessage
+        
+print("key", key)
+message = b'Hello World, hey' # Message is 16 bytes
+cipher = Cipher(algorithm=algorithms.AES(key), mode=modes.ECB())
+
+padded_message = add_padding(message) # Add padding to message
+
+padded_ciphertext = encrypt(padded_message)
+
+padded_decrypted_ciphertext = decrypt(padded_ciphertext)
+
+decrypted_ciphertext = remove_padding(padded_decrypted_ciphertext)
+
+ciphertext = encrypt(message)
+print(ciphertext)
+
+
 
 decryptedtext = decrypt(ciphertext)
 print(decryptedtext)
